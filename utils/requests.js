954e19f9084b9a03145b5bfa6de87f6ae6b2592a
Denis Marginas <denismarginas09@gmail.com>
2024-06-25 15:06:53 +0300

    3. Database, API Routes & Property Components - done

---
+++ utils/requests.js
@@ -0,0 +1,47 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+// Fetch All Properties
+async function fetchProperties() {
+  try {
+    // Handle the case where the domain is not avaible yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${apiDomain}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    return data.properties;
+  } catch (error) {
+    console.error("Error fetching properties:", error);
+    return [];
+  }
+}
+
+// Fetch Single Property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    return data; // Correctly return the single property object
+  } catch (error) {
+    console.error("Error fetching property:", error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
